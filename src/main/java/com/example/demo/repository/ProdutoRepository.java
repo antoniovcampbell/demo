67b4315a19293f5bd336778206e980507bf8093c
antoniovcampbell <antonio.campbell@al.infnet.edu.br>
2022-09-02 20:12:19 -0300

    DemoService

--- src/main/java/com/example/demo/repository/ProdutoRepository.java
+++ src/main/java/com/example/demo/repository/ProdutoRepository.java
@@ -3,7 +3,7 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.pro.aguiar.apiservice.model.Produto;
+import com.example.demo.model.Produto;
 
 @Repository
 public interface ProdutoRepository 
