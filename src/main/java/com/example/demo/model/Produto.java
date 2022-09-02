8aabb8169301bd54a69c90fe10adea3a9d2c9c06
antoniovcampbell <antonio.campbell@al.infnet.edu.br>
2022-09-02 20:28:36 -0300

    save!

--- src/main/java/com/example/demo/model/Produto.java
+++ src/main/java/com/example/demo/model/Produto.java
@@ -1,6 +1,14 @@
 package com.example.demo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "produtos")
 public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String nome;
     private String marca;
