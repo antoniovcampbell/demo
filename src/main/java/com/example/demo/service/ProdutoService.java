7cea1068018711e8c469fe90cf55259776a8bd5a
antoniovcampbell <antonio.campbell@al.infnet.edu.br>
2022-09-02 20:17:04 -0300

    h2 adicionado

---
+++ src/main/java/com/example/demo/service/ProdutoService.java
@@ -0,0 +1,30 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Produto;
+import com.example.demo.repository.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    ProdutoRepository produtoRepository;
+
+    // Listar
+    public List<Produto> listar(){
+        return produtoRepository.findAll();
+    }
+    
+    // criar
+
+    // exibir
+
+    // atualizar
+
+    // excluir
+    
+}
