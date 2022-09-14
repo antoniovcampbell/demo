6615d948c2448eeb15ee94a24e18ba07728e1e88
antoniovcampbell <antonio.campbell@al.infnet.edu.br>
2022-09-14 20:48:34 -0300

    save

---
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @GetMapping
+    public ResponseEntity index(){
+        return ResponseEntity.ok().body("Api Online");
+    }
+    @GetMapping("/index")
+    public ResponseEntity index1(){
+        return ResponseEntity.ok().body("Api Online");
+    }
+}
--- src/main/java/com/example/demo/controller/ProdutosController.java
+++ src/main/java/com/example/demo/controller/ProdutosController.java
@@ -4,6 +4,7 @@ import com.example.demo.model.Produto;
 import com.example.demo.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.PatchMapping;
