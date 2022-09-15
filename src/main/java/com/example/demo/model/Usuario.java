b00531bd4361c987dc968816488ddf523ba9c1c9
antoniovcampbell <antonio.campbell@al.infnet.edu.br>
2022-09-15 08:34:50 -0300

    security

--- src/main/java/com/example/demo/model/Usuario.java
+++ src/main/java/com/example/demo/model/Usuario.java
@@ -1,12 +1,17 @@
 package com.example.demo.model;
 
+import java.util.Collection;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 @Entity(name = "usuarios")
-public class Usuario {
+public class Usuario implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -14,6 +19,15 @@ public class Usuario {
     private String email;
     private String telefone;
     private String cep;
+
+    private String password;
+    private String username;
+    private Boolean enabled = true;
+    private Boolean credentialsNonExpired = true;
+    private Boolean accountNonExpired = true;
+    private Boolean accountNonLocked = true;
+    private String authorities = "USER";
+    
     
     public Usuario(){
 
@@ -70,6 +84,67 @@ public class Usuario {
 
     public void setCep(String cep) {
         this.cep = cep;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+    public Boolean getCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+    public void setCredentialsNonExpired(Boolean credentialsNonExpired) {
+        this.credentialsNonExpired = credentialsNonExpired;
+    }
+    public Boolean getAccountNonExpired() {
+        return accountNonExpired;
+    }
+    public void setAccountNonExpired(Boolean accountNonExpired) {
+        this.accountNonExpired = accountNonExpired;
+    }
+    public Boolean getAccountNonLocked() {
+        return accountNonLocked;
+    }
+    public void setAccountNonLocked(Boolean accountNonLocked) {
+        this.accountNonLocked = accountNonLocked;
+    }
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    @Override
+    public String getPassword() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    @Override
+    public String getUsername() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    @Override
+    public boolean isEnabled() {
+        // TODO Auto-generated method stub
+        return false;
     }
 
     
